3fe695b386d576c43448a328f13b714d33092888
billemaz <maxime.billemaz@nept-in.com>
2019-10-31 12:53:40 +0100

    Add direct wifi

---
+++ app/src/main/java/com/example/customcardgame/PlayerRoomActivity.kt
@@ -0,0 +1,81 @@
+package com.example.customcardgame
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customcardgame.wifi.MySalut
+import com.example.customcardgame.wifi.MySalutCallback
+import com.peak.salut.Callbacks.SalutDataCallback
+import com.peak.salut.SalutDataReceiver
+import com.peak.salut.SalutServiceData
+import android.util.Log
+import androidx.core.app.ActivityCompat
+
+
+class PlayerRoomActivity : AppCompatActivity(), SalutDataCallback {
+
+    lateinit var network: MySalut
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player_room)
+
+
+        ActivityCompat.requestPermissions(this@PlayerRoomActivity,
+            arrayOf(
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.CHANGE_WIFI_STATE,
+                Manifest.permission.CHANGE_NETWORK_STATE,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET
+            ),
+            14541
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 14541){
+            if (grantResults.size == 5
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                && grantResults[3] == PackageManager.PERMISSION_GRANTED
+                && grantResults[4] == PackageManager.PERMISSION_GRANTED)
+            {
+
+                val dataReceiver = SalutDataReceiver(this, this)
+                val serviceData = SalutServiceData("sas", 50489, "USER")
+
+                network = MySalut(dataReceiver, serviceData, MySalutCallback(
+                    this.javaClass.simpleName,
+                    "Sorry, but this device does not support WiFi Direct."
+                ))
+                network.isRunningAsHost = false
+
+                network.discoverNetworkServices({ device ->
+                    Log.d(
+                        this.javaClass.simpleName,
+                        "A device has connected with the name " + device.deviceName
+                    )
+                    network.stopServiceDiscovery(false)
+
+                    network.registerWithHost(
+                        device,
+                        MySalutCallback(this.javaClass.simpleName, "Registered !"),
+                        MySalutCallback(this.javaClass.simpleName, "Registration failed !")
+                    )
+                }, false)
+            }
+        }
+    }
+
+    override fun onDataReceived(p0: Any?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- app/src/main/java/com/example/customcardgame/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/customcardgame/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.customcardgame.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.customcardgame.AdminRoomActivity
+import com.example.customcardgame.PlayerRoomActivity
 import com.example.customcardgame.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 class HomeFragment : Fragment() {
 
@@ -19,13 +24,33 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+            ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+        root.button.setOnClickListener { view ->
+            onUserClick()
+        }
+
+        root.button2.setOnClickListener { view ->
+            onAdminClick()
+        }
+
         return root
+    }
+
+    fun onUserClick() {
+        val intent = Intent(context, PlayerRoomActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onAdminClick() {
+        val intent = Intent(context, AdminRoomActivity::class.java)
+        startActivity(intent)
     }
 }
