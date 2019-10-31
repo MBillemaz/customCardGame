66217631b725b77f05aa22983e6ff0f28a3d5055
Strapontin <antodu73@hotmail.fr>
2019-10-31 12:58:53 +0100

    ajout page de details des cartes

---
+++ app/src/main/java/com/example/customcardgame/ui/cardDetails/CardDetails.kt
@@ -0,0 +1,13 @@
+package com.example.customcardgame.ui.cardDetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customcardgame.R
+
+class CardDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_details)
+    }
+}
