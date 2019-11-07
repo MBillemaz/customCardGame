eac980d1f4a18129631c03b8059c1b0f019daf84
Strapontin <antodu73@hotmail.fr>
2019-11-07 10:19:16 +0100

    Ajout secu login page accueil

--- app/src/main/java/com/example/customcardgame/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/customcardgame/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -26,51 +27,80 @@ class HomeFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+        // Lors du clique sur le bouton de connexion client
         root.userRoomButton.setOnClickListener { view ->
-            onUserClick()
+            onUserClick(context!!)
         }
 
+        // Lors du clique sur le bouton de connexion de l'hôte
         root.adminRoomButton.setOnClickListener { view ->
-            onAdminClick()
+            onAdminClick(context!!)
         }
 
+        // On récupère le login précédemment enregistré par l'utilisateur actuel (si par exemple il a déjà joué)...
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
         val storedLogin = sharedPref?.getString(getString(R.string.login_key), null)
 
+        // ... pour l'afficher dans la textBox
         root.login.setText(storedLogin)
 
         return root
     }
 
-    private fun onUserClick() {
-        storeLogin()
-        val intent = Intent(context, PlayerRoomActivity::class.java)
-        intent.putExtra("login", login.text.toString())
-        startActivity(intent)
+    // Ouvre la page de l'utilisateur client
+    private fun onUserClick(context: Context) {
+
+        // Si le login a été bien enregistré
+        if (storeLogin(context)) {
+
+            // On ouvre la page de l'utilisateur client
+            val intent = Intent(context, PlayerRoomActivity::class.java)
+
+            intent.putExtra("login", login.text.toString())
+            startActivity(intent)
+        }
     }
 
-    private fun onAdminClick() {
-        storeLogin()
-        val intent = Intent(context, AdminRoomActivity::class.java)
-        intent.putExtra("login", login.text.toString())
-        startActivity(intent)
+    // Ouvre la page de l'hôte de la partie
+    private fun onAdminClick(context: Context) {
+
+        // Si le login a été bien enregistré
+        if (storeLogin(context)) {
+
+            // On ouvre la page de l'admin
+            val intent = Intent(context, AdminRoomActivity::class.java)
+
+            intent.putExtra("login", login.text.toString())
+            startActivity(intent)
+        }
     }
 
-    private fun storeLogin() {
+    // Enregistre le login de l'utilisateur
+    private fun storeLogin(context: Context): Boolean {
 
-        val loginText = login.text.toString()
+        // On récupère le login et on enlève les espaces situés au début ou à la fin
+        val loginText = login.text.toString().trim()
 
         // Si l'utilisateur a mal écrit son login
-        if (loginText.isNullOrBlank())
-        {
+        if (loginText.isNullOrBlank()) {
 
-        }
-        else
-        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+            // On affiche une erreur
+            Toast.makeText(context, "Il faut un nom d'utilisateur.", Toast.LENGTH_SHORT)
+                .show()
 
-        with (sharedPref.edit()) {
-            putString(getString(R.string.login_key), loginText)
-            commit()
+            return false
+
+        } else {
+
+            // On enregistre l'identifiant
+            val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return false
+
+            with(sharedPref.edit()) {
+                putString(getString(R.string.login_key), loginText)
+                commit()
+            }
+
+            return true
         }
     }
 }
